d9d8e174382d536fd2afaf8e17798e9a472720b4
codecalm <codecalm@gmail.com>
2015-08-03 19:29:57 +0200

    color lists

--- html/20/1/body.src.php
+++ html/20/1/body.src.php
@@ -1,2 +1,5 @@
-<?php
-
+<ul>
+<?php foreach(sample::getColors() as $color): ?>
+	<li><div class="color" style="background-color: <?= $color ?>"></div> <?= $color ?></li>
+<?php endforeach; ?>
+</ul>
